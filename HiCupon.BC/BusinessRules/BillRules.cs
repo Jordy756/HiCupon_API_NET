e3ab3c59e9fbfe4974e91e50f707fa508737d608
Nicole <82064048+NicoleAlvarado@users.noreply.github.com>
2024-06-10 14:11:58 -0600

    Business Rules BC
    Business Rules BC

---
+++ HiCupon.BC/BusinessRules/BillRules.cs
@@ -0,0 +1,26 @@
+﻿using HiCupon.BC.Models;
+
+namespace HiCupon.BC.BusinessRules
+{
+    public class BillRules
+    {
+        public static bool ValidateBill(Bill bill)
+        {
+            if (bill.User.Id <= 0) throw new Exception("El id del usuario no puede ser menor o igual a 0");
+            if (bill.BasePrice <= 0) throw new Exception("El precio base de la factura no puede ser menor o igual a 0");
+            if (bill.Iva <= 0) throw new Exception("El IVA de la factura no puede ser menor o igual a 0");
+            if (bill.Total <= 0) throw new Exception("El precio total de la factura no puede ser menor o igual a 0");
+            
+            return true;
+        }
+
+        /*
+        public static bool ValidateUserId(int userId)
+        {
+            if (userId <= 0) throw new Exception("El id del customer no puede ser menor o igual a 0");
+
+            return true;
+        }
+        */
+    }
+}
---
+++ HiCupon.BC/BusinessRules/UserRules.cs
@@ -0,0 +1,34 @@
+﻿using HiCupon.BC.Models;
+
+namespace HiCupon.BC.BusinessRules
+{
+    public class UserRules
+    {
+        public static bool ValidateUser(User user)
+        {
+            if (string.IsNullOrEmpty(user.Name)) throw new Exception("El nombre del cliente no puede ir vacío");
+            if (string.IsNullOrEmpty(user.LastName)) throw new Exception("El apellido del cliente no puede ir vacío");
+            if (string.IsNullOrEmpty(user.IdentificationCard)) throw new Exception("La identificación del cliente no puede ir vacía");
+            if (user.DateBirth == DateTime.MinValue) throw new Exception("La fecha de nacimiento del cliente no puede ir vacía");
+
+            return true;
+        }
+
+        public static bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) throw new Exception("El email del cliente no puede ir vacío");
+            if (!email.Contains("@")) throw new Exception("El email del cliente no es válido");
+
+            return true;
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password)) throw new Exception("La contraseña del cliente no puede ir vacía");
+            if (password.Length < 8) throw new Exception("La contraseña del cliente debe tener al menos 8 caracteres");
+            if (password.Length >= 12) throw new Exception("La contraseña del cliente no puede tener más de 12 caracteres");
+
+            return true;
+        }
+    }
+}
